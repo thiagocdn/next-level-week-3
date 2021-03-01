e61cf35621205ebc11993101a9d38e78d6f2d269
Thiago Cedran Santos <thiagocdn@gmail.com>
2021-02-28 21:06:50 -0300

    Adjusted timming

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -21,7 +21,7 @@ export const CountdownContext = createContext({} as CountdownContextData);
 export function CountdownProvider({ children }: CountdownProviderProps) {
   const { startNewChallenge } = useContext(ChallengesContext);
 
-  const [ time, setTime ] = useState(3);
+  const [ time, setTime ] = useState(25 * 60);
   const [ isActive, setIsActive ] = useState(false);
   const [ hasFinished, setHasFinished ] = useState(false);
 
